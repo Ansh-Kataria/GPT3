1a597bdf68638f8369b5a627f1b8c51c6e7d5a9b
Ansh Kataria <anshkataria28@gmail.com>
2023-01-09 15:32:08 +0530

    brand section created

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return <div className="gpt3__whatgpt3 section__margin" id="wgpt3">WhatGPT3</div>;
 };
 
 export default WhatGPT3;
